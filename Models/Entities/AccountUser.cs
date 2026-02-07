a2a122b57a85f165b744f2a25e676f40a4bfd062
SindhujaBakka <sindhu9040f@gmail.com>
2026-02-07 19:49:41 +0530

    Initial Commit

---
+++ Models/Entities/AccountUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class AccountUser
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Guid AccountId { get; private set; }
+        public string Username { get; private set; } = default!;
+        public string UsernameNormalized { get; private set; } = default!;
+
+        public DateTimeOffset CreateDateTime { get; private set; }
+        public DateTimeOffset UpdatedDateTime { get; private set; }
+    }
+}
